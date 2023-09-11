8f0b825ab137b45a8ce97f231d133d97b5b509ed
mahmood jalilnejad <m.jalilnezhad@gmail.com>
2023-09-11 16:46:59 +0330

    fix conditional precedence in IsTollFreeDate checking
    add and enhancement comment

--- src/CongestionTaxCalculator.cs
+++ src/CongestionTaxCalculator.cs
@@ -76,16 +76,17 @@ public class CongestionTaxCalculator
 
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
 
+        // enhancement : may be later we can store holidays in the database
         if (year == 2013)
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
+            if ((month == 1 && day == 1) ||
+                (month == 3 && (day == 28 || day == 29)) ||
+                (month == 4 && (day == 1 || day == 30)) ||
+                (month == 5 && (day == 1 || day == 8 || day == 9)) ||
+                (month == 6 && (day == 5 || day == 6 || day == 21)) ||
+                (month == 7) ||
+                (month == 11 && day == 1) ||
+                (month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)))
             {
                 return true;
             }
